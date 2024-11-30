c82d0ae2b834f3c29ea4eb3acd7b2068632bf1db
Quan <doquan020903@gmail.com>
2024-11-30 23:40:36 +0700

    feat: continue payment service

--- VioVid.WebAPI/Services/PaymentService.cs
+++ VioVid.WebAPI/Services/PaymentService.cs
@@ -31,6 +31,114 @@ public class PaymentService : IGenreService
         return genre;
     }
 
+    public async Task<Genre> CreateaGenreAsync(CreateGenreRequest createGenreRequest)
+    {
+        var newGenre = new Genre()
+        {
+            Name = createGenreRequest.Name,
+        };
+        await _dbContext.Genres.AddAsync(newGenre);
+        await _dbContext.SaveChangesAsync();
+        return newGenre;
+    }
+
+    public async Task<Genre> UpdateGsdenreAsync(Guid id, UpdateGenreRequest updateGenreRequest)
+    {
+        var genre = await _dbContext.Genres.FindAsync(id);
+        if (genre == null)
+        {
+            throw new NotFoundException($"Không tìm thấy Genre có id {id}");
+        }
+        genre.Name = updateGenreRequest.Name;
+        await _dbContext.SaveChangesAsync();
+
+        return genre;
+    }
+
+
+    public async Task<Guid> DeleteGefdasnreAsync(Guid id)
+    {
+        var genre = await _dbContext.Genres.FindAsync(id);
+        if (genre == null)
+        {
+            throw new NotFoundException($"Không tìm thấy Genre có id {id}");
+        }
+        _dbContext.Genres.Remove(genre);
+        await _dbContext.SaveChangesAsync();
+        return id;
+    }
+    public async Task<Genre> CreateGefdsafnreAsync(CreateGenreRequest createGenreRequest)
+    {
+        var newGenre = new Genre()
+        {
+            Name = createGenreRequest.Name,
+        };
+        await _dbContext.Genres.AddAsync(newGenre);
+        await _dbContext.SaveChangesAsync();
+        return newGenre;
+    }
+
+    public async Task<Genre> UpdateGenrfdsaeAsync(Guid id, UpdateGenreRequest updateGenreRequest)
+    {
+        var genre = await _dbContext.Genres.FindAsync(id);
+        if (genre == null)
+        {
+            throw new NotFoundException($"Không tìm thấy Genre có id {id}");
+        }
+        genre.Name = updateGenreRequest.Name;
+        await _dbContext.SaveChangesAsync();
+
+        return genre;
+    }
+
+
+    public async Task<Guid> DeleteGenfdsafeAsync(Guid id)
+    {
+        var genre = await _dbContext.Genres.FindAsync(id);
+        if (genre == null)
+        {
+            throw new NotFoundException($"Không tìm thấy Genre có id {id}");
+        }
+        _dbContext.Genres.Remove(genre);
+        await _dbContext.SaveChangesAsync();
+        return id;
+    }
+    public async Task<Genre> CreateGenrfdsafeAsync(CreateGenreRequest createGenreRequest)
+    {
+        var newGenre = new Genre()
+        {
+            Name = createGenreRequest.Name,
+        };
+        await _dbContext.Genres.AddAsync(newGenre);
+        await _dbContext.SaveChangesAsync();
+        return newGenre;
+    }
+
+    public async Task<Genre> UpdateGenfdsafdreAsync(Guid id, UpdateGenreRequest updateGenreRequest)
+    {
+        var genre = await _dbContext.Genres.FindAsync(id);
+        if (genre == null)
+        {
+            throw new NotFoundException($"Không tìm thấy Genre có id {id}");
+        }
+        genre.Name = updateGenreRequest.Name;
+        await _dbContext.SaveChangesAsync();
+
+        return genre;
+    }
+
+
+    public async Task<Guid> DeleteGefdsafnreAsync(Guid id)
+    {
+        var genre = await _dbContext.Genres.FindAsync(id);
+        if (genre == null)
+        {
+            throw new NotFoundException($"Không tìm thấy Genre có id {id}");
+        }
+        _dbContext.Genres.Remove(genre);
+        await _dbContext.SaveChangesAsync();
+        return id;
+    }
     public async Task<Genre> CreateGenreAsync(CreateGenreRequest createGenreRequest)
     {
         var newGenre = new Genre()
