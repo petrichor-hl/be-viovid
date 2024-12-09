791dcdbe3a1f252265eafa4c3a41cb8cbd93ded5
Quan <doquan020903@gmail.com>
2024-12-09 23:11:46 +0700

    feat: update Program.cs

--- VioVid.WebAPI/Program.cs
+++ VioVid.WebAPI/Program.cs
@@ -50,7 +50,10 @@ builder.Services.AddTransient<IJwtService, JwtService>()
     .AddScoped<IPersonService, PersonService>()
     .AddScoped<ITopicService, TopicService>()
     .AddScoped<IFilmService, FilmService>()
-    .AddScoped<IUserService, UserService>();
+    .AddScoped<IUserService, UserService>()
+    .AddScoped<IPostService, PostService>()
+    .AddScoped<IPostCommentService, PostCommentService>()
+    .AddScoped<IChannelService, ChannelService>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
